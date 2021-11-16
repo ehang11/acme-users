50b1e6f6eb3d76d52de9c13ee2696372e724ffd1
Eric Hang <ehang11@gmail.com>
2021-11-15 19:51:52 -0500

    first commit

--- server.js
+++ server.js
@@ -1,8 +1,14 @@
 const express = require('express');
 const path = require('path');
 
+const faker = require('faker');
 const app = express();
-
+//const morgan = require('morgan');
+//app.get(morgan('dev));
+app.use('/dist', express.static(path.join(__dirname, 'dist')));
+app.use('/src', express.static(path.join(__dirname, 'src')));
 app.get('/', (req, res)  => res.sendFile(path.join(__dirname, 'index.html')));
-const port = process.env.port || 3000;
+const port = process.env.port || 4000;
 
+app.listen(port, ()=>console.log(`listening on port ${port}`));
+//console.log(faker.helpers.userCard());
---
+++ src/index.js
@@ -0,0 +1,23 @@
+//modify your index.js file to generate a collection of 50 users (see faker.helpers.userCard() method)
+const faker = require('faker');
+console.log(faker.helpers.userCard());
+
+
+const renderHTML = ()=> {
+    const html //=(numbers.map(num => 
+    /*return `<li>
+      <a href=`#${num}`> ${num} </a>
+    </li>
+    `;
+}).join('');
+ul.innerHTML = html;
+    */
+
+
+
+
+
+
+};
+//reccommended to add window.addEventListener( 'hashchange', ()=> { renderHTML(); });
+//renderHTML();
